a511fa038baa29f3653fe1169b51a95c403dde00
Igor Simko <simko.igor@gmail.com>
2017-11-28 23:05:15 +0100

    test whole dataset

--- 03-Twitter-chatbot.py
+++ 03-Twitter-chatbot.py
@@ -1,4 +1,3 @@
-
 # In[1]:
 
 # preprocessed data
@@ -14,7 +13,7 @@ idx_a, idx_q = metadata['idx_headings'], metadata['idx_descriptions']
 # parameters 
 xseq_len = trainX.shape[-1]
 yseq_len = trainY.shape[-1]
-batch_size = 1
+batch_size = 32
 xvocab_size = len(metadata['idx2word'])
 yvocab_size = xvocab_size
 emb_dim = 300
@@ -22,7 +21,7 @@ emb_dim = 300
 import seq2seq_wrapper
 
 # In[7]:
-
+#
 model = seq2seq_wrapper.Seq2Seq(xseq_len=xseq_len,
                                yseq_len=yseq_len,
                                xvocab_size=xvocab_size,
@@ -30,29 +29,30 @@ model = seq2seq_wrapper.Seq2Seq(xseq_len=xseq_len,
                                ckpt_path='ckpt/twitter/',
                                emb_dim=emb_dim,
                                num_layers=3,
-                                epochs=20
+                                epochs=100
                                )
 
 
 # In[8]:
 # 32, 256
-val_batch_gen = data_utils.rand_batch_gen(validX, validY, 1)
+val_batch_gen = data_utils.rand_batch_gen(validX, validY, 32)
 train_batch_gen = data_utils.rand_batch_gen(trainX, trainY, batch_size)
-test_batch_gen = data_utils.rand_batch_gen(testX, testY, 1)
+test_batch_gen = data_utils.rand_batch_gen(testX, testY, 5)
 
 # In[9]:
 # sess = model.restore_last_session()
 sess = model.train(train_batch_gen, val_batch_gen)
 
-input_ = test_batch_gen.__next__()[0]
-output = model.predict(sess, input_)
-print(output.shape)
+for x in range(len(testX)):
+    input_ = test_batch_gen.__next__()[0]
+    output = 0  # model.predict(sess, input_)
+    # print(output.shape)
 
-replies = []
-for ii, oi in zip(input_.T, output):
-    q = data_utils.decode(sequence=ii, lookup=metadata['idx2word'], separator=' ')
-    decoded = data_utils.decode(sequence=oi, lookup=metadata['idx2word'], separator=' ').split(' ')
-    if decoded.count('unk') == 0:
+    replies = []
+    for ii, oi in zip(input_.T, output):
+        q = data_utils.decode(sequence=ii, lookup=metadata['idx2word'], separator=' ')
+        decoded = data_utils.decode(sequence=oi, lookup=metadata['idx2word'], separator=' ').split(' ')
+        # if decoded.count('unk') == 0:
         if decoded not in replies:
             print('q : [{0}]; a : [{1}]'.format(q, ' '.join(decoded)))
             replies.append(decoded)
--- seq2seq_wrapper.py
+++ seq2seq_wrapper.py
@@ -144,7 +144,7 @@ class Seq2Seq(object):
         for i in range(self.epochs):
             try:
                 self.train_batch(sess, train_set)
-                if i and i% (10) == 0: # TODO : make this tunable by the user
+                if i and i% (1) == 0: # TODO : make this tunable by the user
                     # save model to disk
                     #saver.save(sess, self.ckpt_path + self.model_name + '.ckpt', global_step=i)
                     # evaluate to get validation loss
